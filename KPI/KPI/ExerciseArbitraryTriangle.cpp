7af7541cfb00ec22cd192562cbed4bfc29a7f3ff
Valid <validhernufKPI@gmail.com>
2021-05-11 23:23:10 +0300

    Add second exercices in Arbitrary triangle

--- KPI/KPI/ExerciseArbitraryTriangle.cpp
+++ KPI/KPI/ExerciseArbitraryTriangle.cpp
@@ -14,9 +14,25 @@ std::vector<float> ExerciseArbitraryTriangle::first()
 	}
 	while((firstParameter+secondParameter<thirdParameter)||(thirdParameter+secondParameter<firstParameter)||(thirdParameter+firstParameter<secondParameter));
 	ViewMessages::ConditionOut("Заданий довільний трикутник ABC. Знайти невідомі кути трикутника за стороною AB = " +
-		to_string(firstParameter) + "см,  BC=" + to_string(secondParameter) + "cм, AC = " + to_string(thirdParameter) + "см.");
-	results.push_back(round(acos((pow(firstParameter, 2)+pow(secondParameter, 2)-pow(thirdParameter, 2))/(2*firstParameter*secondParameter))*10) / 10.0); //Кут B
-	results.push_back(round(acos((pow(thirdParameter, 2)+pow(secondParameter, 2)-pow(firstParameter, 2))/(2*thirdParameter*secondParameter))*10) / 10.0); //Кут C
+		to_string(firstParameter) + "см,  BC = " + to_string(secondParameter) + "cм, AC = " + to_string(thirdParameter) + "см.");
+	results.push_back(round(acos((pow(firstParameter, 2)+pow(secondParameter, 2)-pow(thirdParameter, 2))/(2 * firstParameter * secondParameter))*10) / 10.0); //Кут B
+	results.push_back(round(acos((pow(thirdParameter, 2)+pow(secondParameter, 2)-pow(firstParameter, 2))/(2 * thirdParameter * secondParameter))*10) / 10.0); //Кут C
 	results.push_back(180-results[0]-results[1]); //Кут A
+	return results;
+}
+
+std::vector<float> ExerciseArbitraryTriangle::second()
+{
+	firstParameter = rand() % 20 + 10; //Сторона AB
+	secondParameter = rand() % 20 + 10; //Сторона BC
+	do
+	{
+		thirdParameter = rand() % 130 + 20; //кут B
+	} while (thirdParameter == 90);
+	ViewMessages::ConditionOut("Заданий довільний трикутник ABC. Знайти невідомі кути трикутника за стороною AB = " +
+		to_string(firstParameter) + "см, ВС = " + to_string(secondParameter) + "см та кут В = " + to_string(thirdParameter) + '.');
+	results.push_back(round(sqrt(pow(firstParameter,2)+pow(secondParameter,2) - 2 * firstParameter * secondParameter * cos(thirdParameter * M_PI / 180)) * 10)/10.0); //Сторона AC
+	results.push_back(round(acos((pow(results[0], 2) + pow(secondParameter, 2) - pow(firstParameter, 2)) / (2 * results[0] * secondParameter)) * 10) / 10.0); //Кут С
+	results.push_back(180 - thirdParameter - results[1]); //Кут А
 	return results;
 }
