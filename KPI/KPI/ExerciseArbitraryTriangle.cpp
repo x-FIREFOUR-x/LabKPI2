e0aeaf262644eb22f191288ecd13005ae2581440
Oleksandr <OlexandrTanasienko@gmail.com>
2021-05-11 22:28:57 +0300

    First task three sides arbitrary triangle

--- KPI/KPI/ExerciseArbitraryTriangle.cpp
+++ KPI/KPI/ExerciseArbitraryTriangle.cpp
@@ -1,1 +1,22 @@
 #include "ExerciseArbitraryTriangle.h"
+#include "ViewMessages.h"
+#include <cmath>
+
+using namespace std;
+
+std::vector<float> ExerciseArbitraryTriangle::first()
+{
+	firstParameter = rand() % 20 + 10; //сторона AB
+	secondParameter = rand() % 20 + 10; //сторона BC
+	do
+	{
+		thirdParameter = rand() % 20 + 13; //сторона AC
+	}
+	while((firstParameter+secondParameter<thirdParameter)||(thirdParameter+secondParameter<firstParameter)||(thirdParameter+firstParameter<secondParameter));
+	ViewMessages::ConditionOut("Заданий довільний трикутник ABC. Знайти невідомі кути трикутника за стороною AB = " +
+		to_string(firstParameter) + "см,  BC=" + to_string(secondParameter) + "cм, AC = " + to_string(thirdParameter) + "см.");
+	results.push_back(round(acos((pow(firstParameter, 2)+pow(secondParameter, 2)-pow(thirdParameter, 2))/(2*firstParameter*secondParameter))*10) / 10.0); //Кут B
+	results.push_back(round(acos((pow(thirdParameter, 2)+pow(secondParameter, 2)-pow(firstParameter, 2))/(2*thirdParameter*secondParameter))*10) / 10.0); //Кут C
+	results.push_back(180-results[0]-results[1]); //Кут A
+	return results;
+}
