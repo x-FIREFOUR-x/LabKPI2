8c1d7dcdc371692dca7973afc48f45d7142acf26
Oleksandr <OlexandrTanasienko@gmail.com>
2021-05-13 01:39:06 +0300

    Fix rounding in exercises for right triangle

--- KPI/KPI/ExerciseRightTriangle.cpp
+++ KPI/KPI/ExerciseRightTriangle.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <cmath>
 
-# define M_PI           3.14159265358979323846
+#define M_PI 3.14159265358979323846
 
 using namespace std;
 
@@ -11,12 +11,14 @@ vector<float> ExerciseRightTriangle::first()
 {
     int AB = rand() % 20 + 10; //hypotenuse
     int BC = rand() % (AB - 9) + 5; //cathetus
-    int C = 90; //right angle
     ViewMessages::ConditionOut("Заданий прямокутний трикутник ABC. Знайти невідомі сторони й гострі кути трикутника за гіпотенузою AB = " +
         to_string(AB) + "см і катетом BC=" + to_string(BC) + "cм.");
-    results.push_back(round(asin((((float)BC) / AB)) * 1800.0 / M_PI) / 10.0); //angle A
-    results.push_back(90 - results[0]); //angle B
-    results.push_back(round(sqrt(pow(AB, 2) - pow(BC, 2)) * 10) / 10.0); //cathetus АС
+	float A=asin((((float)BC) / AB)) * 180.0 / M_PI;
+    float B=90-A;
+	float AC=sqrt(pow(AB, 2) - pow(BC, 2));
+    results.push_back(round( A*10)/ 10.0);
+    results.push_back(round( B*10)/ 10.0);
+    results.push_back(round(AC * 10) / 10.0);
     return results;
 }
 
@@ -24,12 +26,14 @@ vector<float> ExerciseRightTriangle::second()
 {
     int AC = rand() % 20 + 5; //cathetus
     int BC = rand() % 20 + 5; //cathetus
-    int C = 90;
     ViewMessages::ConditionOut("Заданий прямокутний трикутник ABC. Знайти гіпотенузу та гострі кути трикутника за катетом AC = " +
         to_string(AC) + "см і катетом BC = " + to_string(BC) + "см.");
-    results.push_back(round(sqrt(pow(AC, 2) + pow(BC, 2)) * 10) / 10.0); //hypotenuse AB
-    results.push_back(round(asin((((float)BC) / results[0])) * 1800.0 / M_PI) / 10.0); //angle А
-    results.push_back(90 - results[1]); //angle B
+	float AB=sqrt(pow(AC, 2) + pow(BC, 2));//hypotenuse
+	float A=asin((((float)BC) / AB)) * 180.0 / M_PI;
+	float B=90-A;
+    results.push_back(round( AB* 10) / 10.0);
+    results.push_back(round(A)/ 10.0);
+    results.push_back(B);
     return results;
 }
 
@@ -37,13 +41,15 @@ vector<float> ExerciseRightTriangle::third()
 {
     int AB = rand() % 30; //cathetus
     int A = rand() % 70 + 10; //adjacent acute angle А
-    int B = 90;
     float angleInRadians = (static_cast<float>(A)) * M_PI / 180;
     ViewMessages::ConditionOut("Заданий прямокутний трикутник ABC. Знайти невідомі сторони й гострі кути трикутника за катетом AB = " +
         to_string(AB) + "см і гострим кутом А = " + to_string(A) + '.');
-    results.push_back(round((static_cast<float>(AB)) / cos(angleInRadians) * 10) / 10.0); // hypotenuse АС
-    results.push_back(round(static_cast<float>(AB) * tan(angleInRadians) * 10) / 10.0); // cathetus ВС
-    results.push_back(90 - A); // acute angle С
+	float AC=static_cast<float>(AB) / cos(angleInRadians);//hypotenuse
+	float BC=static_cast<float>(AB) * tan(angleInRadians);//cathetus
+	float C=90 - A;                                       // acute angle
+    results.push_back(round( AC* 10) / 10.0);
+    results.push_back(round( BC* 10) / 10.0);
+    results.push_back(C);
     return results;
 }
 
@@ -51,15 +57,15 @@ vector<float> ExerciseRightTriangle::fourth()
 {
     int AB = rand() % 30; //cathetus АВ
     int C = rand() % 70 + 10; //opposite acute angle C
-    int B = 90; //right angle В
     float angleInRadians = (static_cast<float>(C)) * M_PI / 180;
-
     ViewMessages::ConditionOut("Знайти невідомі сторони й гострі кути прямокутного трикутника за катетом AB=" +
         to_string(AB) + " і гострим кутом С=" + to_string(C) + '.');
-    results.push_back((static_cast<float>(AB)) / sin(angleInRadians)); //hypotenuse АС
-    results.push_back(90 - C); //acute angle А
-    angleInRadians = (results[1]) * M_PI / 180;
-    results.push_back(static_cast<float>(AB) * tan(angleInRadians)); //cathetus ВС
+	float AC=(static_cast<float>(AB)) / sin(angleInRadians);//hypotenuse
+	float A=90-C;                                           //acute angle
+    float BC=static_cast<float>(AB) * tan( (A) * M_PI / 180);//cathetus
+    results.push_back(AC);
+    results.push_back(A);
+    results.push_back(BC);
     return results;
 }
 
@@ -67,12 +73,14 @@ vector<float> ExerciseRightTriangle::fifth()
 {
     int AB = rand() % 20 + 10; //hypotenuse
     int A = rand() % 70 + 10; //adjacent acute angle
-    int C = 90;
     float angleInRadians = (static_cast<float>(A)) * M_PI / 180;
     ViewMessages::ConditionOut("Заданий прямокутник трикутник ABC. Знайти катети та невідомий гострий кут трикутника за гіпотенузою AB = " +
         to_string(AB) + "см і прилеглим до нього гострим кутом А = " + to_string(A) + '.');
-    results.push_back(round((float)AB * cos(angleInRadians) * 10) / 10.0); //cathetus АС
-    results.push_back(round((float)AB * sin(angleInRadians) * 10) / 10.0); //cathetus ВС
-    results.push_back(90 - A); //cathetus В
+	float AC=(static_cast<float> (AB)) * cos(angleInRadians);       //cathetus
+	float BC=(static_cast<float> (AB)) * sin(angleInRadians);       //cathetus
+	float B=90-A;
+    results.push_back(round( AC* 10) / 10.0);
+    results.push_back(round( BC* 10) / 10.0);
+    results.push_back(B);
     return results;
 }
