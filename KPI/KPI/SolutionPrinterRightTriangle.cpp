d87600a4e3bc48fecbf539484240c0c34a59ffeb
Oleksandr <OlexandrTanasienko@gmail.com>
2021-05-26 22:55:36 +0300

    Fix messages SolutionsPrinterRightTriangle, use methods of its class in WorkWithTests

--- KPI/KPI/SolutionPrinterRightTriangle.cpp
+++ KPI/KPI/SolutionPrinterRightTriangle.cpp
@@ -6,35 +6,35 @@ using namespace std;
 
 void SolutionPrinterRightTriangle::printFirstSolution(vector<float> results)
 {
-	cout<<"A=arcsin(BC/AB)*180.0/PI="<<results[0]<<endl;
-	cout<<"B=90-A="<<results[1]<<endl;
-	cout<<"AC=sqrt(AB^2-BC^2)="<<results[2]<<endl;
+	cout<<"Angle A=arcsin(BC/AB)*180.0/PI="<<results[0]<<endl;
+	cout<<"Angle B=90-A="<<results[1]<<endl;
+	cout<<"Cathet AC=sqrt(AB^2-BC^2)="<<results[2]<<endl;
 }
 
 void SolutionPrinterRightTriangle::printSecondSolution(vector<float> results)
 {
-	cout<<"AB=sqrt(AC^2+BC^2)="<<results[0]<<endl;
-	cout<<"A=asin(BC/AB)*180.0/PI="<<results[1]<<endl;
-	cout<<"B=90-A="<<results[2]<<endl;
+	cout<<"Hypothenuse AB=sqrt(AC^2+BC^2)="<<results[0]<<endl;
+	cout<<"Angle A=arcsin(BC/AB)*180.0/PI="<<results[1]<<endl;
+	cout<<"Angle B=90-A="<<results[2]<<endl;
 }
 
 void SolutionPrinterRightTriangle::printThirdSolution(std::vector<float> results)
 {
-	cout<<"AC=AB/cos(A)="<<results[0]<<endl;
-	cout<<"BC=AB*tang(A)="<<results[1]<<endl;
-	cout<<"C=90-A="<<results[2]<<endl;
+	cout<<"Hypothenuse AC=AB/cos(A)="<<results[0]<<endl;
+	cout<<"Cathet BC=AB*tang(A)="<<results[1]<<endl;
+	cout<<"Angle C=90-A="<<results[2]<<endl;
 }
 
 void SolutionPrinterRightTriangle::printFourthSolution(std::vector<float> results)
 {
-	cout<<"AC="<<results[0]<<endl;
-	cout<<"A=90-C="<<results[1]<<endl;
-	cout<<"BC=AB*tang(a)="<<results[2]<<endl;
+	cout<<"Hypothenuse AC="<<results[0]<<endl;
+	cout<<"Angle A=90-C="<<results[1]<<endl;
+	cout<<"Cathet BC=AB*tang(a)="<<results[2]<<endl;
 }
 
 void SolutionPrinterRightTriangle::printFifthSolution(std::vector<float> results)
 {
-	cout<<"AC=AB*cos(A)="<<results[0]<<endl;
-	cout<<"BC=AB*sin(A)="<<results[1]<<endl;
-	cout<<"B=90-A="<<results[2]<<endl;
+	cout<<"Cathet AC=AB*cos(A)="<<results[0]<<endl;
+	cout<<"Cathet BC=AB*sin(A)="<<results[1]<<endl;
+	cout<<"Angle B=90-A="<<results[2]<<endl;
 }
