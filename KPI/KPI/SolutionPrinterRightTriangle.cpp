33db16210fc685733be320841ed4b852ba3883fa
Oleksandr <OlexandrTanasienko@gmail.com>
2021-05-26 22:38:07 +0300

    Implement all methods to get solutions of tasks about right triangle

--- KPI/KPI/SolutionPrinterRightTriangle.cpp
+++ KPI/KPI/SolutionPrinterRightTriangle.cpp
@@ -10,10 +10,31 @@ void SolutionPrinterRightTriangle::printFirstSolution(vector<float> results)
 	cout<<"B=90-A="<<results[1]<<endl;
 	cout<<"AC=sqrt(AB^2-BC^2)="<<results[2]<<endl;
 }
-/*
-void SolutionPrinterRightTriangle::printSecondSolution(vector<float> taskData)
+
+void SolutionPrinterRightTriangle::printSecondSolution(vector<float> results)
 {
-	cout<<"A=arcsin("<<taskData[4]<<"/"<<taskData[3]<<")*180.0/PI="<<taskData[0]<<endl;
-	cout<<"B=90-A"<<taskData[0]<<"="<<taskData[1]<<endl;
-	cout<<"AC=sqrt("<<taskData[3]<<"^2-"<<taskData[4]<<"^2)="<<taskData[2]<<endl;
-}*/
+	cout<<"AB=sqrt(AC^2+BC^2)="<<results[0]<<endl;
+	cout<<"A=asin(BC/AB)*180.0/PI="<<results[1]<<endl;
+	cout<<"B=90-A="<<results[2]<<endl;
+}
+
+void SolutionPrinterRightTriangle::printThirdSolution(std::vector<float> results)
+{
+	cout<<"AC=AB/cos(A)="<<results[0]<<endl;
+	cout<<"BC=AB*tang(A)="<<results[1]<<endl;
+	cout<<"C=90-A="<<results[2]<<endl;
+}
+
+void SolutionPrinterRightTriangle::printFourthSolution(std::vector<float> results)
+{
+	cout<<"AC="<<results[0]<<endl;
+	cout<<"A=90-C="<<results[1]<<endl;
+	cout<<"BC=AB*tang(a)="<<results[2]<<endl;
+}
+
+void SolutionPrinterRightTriangle::printFifthSolution(std::vector<float> results)
+{
+	cout<<"AC=AB*cos(A)="<<results[0]<<endl;
+	cout<<"BC=AB*sin(A)="<<results[1]<<endl;
+	cout<<"B=90-A="<<results[2]<<endl;
+}
--- KPI/KPI/SolutionPrinterRightTriangle.h
+++ KPI/KPI/SolutionPrinterRightTriangle.h
@@ -5,9 +5,9 @@ class SolutionPrinterRightTriangle
 {
 public:
 	static void printFirstSolution(std::vector<float> results);
-	//static void printSecondSolution(std::vector<float> taskData);
-	//static void printThirdSolution(std::vector<float> taskData);
-	//static void printFourthSolution(std::vector<float> taskData);
-	//static void printFifthSolution(std::vector<float> taskData);
+	static void printSecondSolution(std::vector<float> results);
+	static void printThirdSolution(std::vector<float> results);
+	static void printFourthSolution(std::vector<float> results);
+	static void printFifthSolution(std::vector<float> results);
 };
 
